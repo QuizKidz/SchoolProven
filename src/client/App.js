1899050d460f8b0ebfe9223895c2633b160285ea
Luis Calona <calona.luis@gmail.com>
2020-02-03 17:07:57 -0800

    Added bootstrap

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/app.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 import SchoolImage from './img/school.png';
 
 export default function App() {
   return (
     <div>
-      <h2>Welcome to SchoolProven!</h2>
+      <Jumbotron>
+        <h1>Welcome to SchoolProven</h1>
+        <p>
+          Research your classes with confidence!
+        </p>
+        <p>
+          <Button variant="primary">Get Started</Button>
+        </p>
+      </Jumbotron>
       <img src={SchoolImage} alt="School" />
     </div>
   );
