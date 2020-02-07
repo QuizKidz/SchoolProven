d10d66404d932bb08e2d4171acb111f888e675cb
Luis Calona <calona.luis@gmail.com>
2020-02-06 22:22:52 -0800

    Updated NavBar to use Link

--- src/client/components/shared/NavBar.js
+++ src/client/components/shared/NavBar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from './Logo';
 
 export default function NavBar() {
   return (
     <Navbar bg="light">
-      <Navbar.Brand href="/">
-        <Logo />
-        SchoolProven
-      </Navbar.Brand>
+      <Link to="/">
+        <Navbar.Brand>
+          <Logo />
+          SchoolProven
+        </Navbar.Brand>
+      </Link>
     </Navbar>
   );
 }
