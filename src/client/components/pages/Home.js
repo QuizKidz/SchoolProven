16ec4573add2ed711b90b554e0c0f7c6396f39ab
Luis Calona <calona.luis@gmail.com>
2020-02-27 22:22:50 -0800

    Fixed bad link

--- src/client/components/pages/Home.js
+++ src/client/components/pages/Home.js
@@ -65,7 +65,7 @@ export default function Home(props) {
 
   const renderLoggedInOptions = () => (
     <>
-      <Link to={`/profile_experiment/${loggedInUser.id}`}>
+      <Link to={`/profile/${loggedInUser.id}`}>
         <Button variant="primary" size="lg" block>
           <FaUserGraduate />
           {' '}
